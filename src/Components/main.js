ae66ef7e5e3f30a106bc4456f9ef09ca69fd4592
Kevin Zhu <kevinzhu@Kevins-Air.cpe.cableonda.net>
2022-07-14 16:17:27 -0500

    Implement routing to alternate main component page

--- src/Components/main.js
+++ src/Components/main.js
@@ -1,12 +1,14 @@
 import Home from './Pages/home'
 import Contact from './Pages/contact'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Product from './Pages/product'
+import { Routes, Route } from 'react-router-dom'
 
 const Main = () => {
     return(
         <div>
             <Routes>
                 <Route path="/" element={<Home/>}/>
+                <Route path="/product" element={<Product/>}/>
                 <Route path="/contact" element={<Contact/>}/>
             </Routes>
         </div>
